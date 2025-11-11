6c08fdf0b66a75a5f2473c21d4b013ca91c12479
GameDev694 <ilia694@mail.ru>
2025-11-11 20:42:49 +0300

    Update Program.cs

--- Connectle/Program.cs
+++ Connectle/Program.cs
@@ -2,17 +2,14 @@ using Connectle.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем SignalR
 builder.Services.AddSignalR();
-
-// Настройка CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", policy =>
     {
         policy.AllowAnyHeader()
               .AllowAnyMethod()
-              .AllowAnyOrigin()  // Разрешаем все источники
+              .AllowAnyOrigin()
               .AllowCredentials();
     });
 });
@@ -23,6 +20,5 @@ app.UseCors("CorsPolicy");
 app.UseStaticFiles();
 app.MapHub<ChatHub>("/chatHub");
 
-// Для Render
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Run($"http://0.0.0.0:{port}");
